83bf64d97d18c3bc091e4fb9959b479eb9324540
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-08-22 15:38:40 -0300

    últimas inserções no código JS para finalização do projeto

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-//enquanto chute não for igual ao n.s
+//enquanto chute não for igual ao numero secreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
-
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (numeroSecreto == chute) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O numero secreto é menor que ${chute}`);
@@ -21,3 +21,12 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);    
+// }
