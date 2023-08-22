c3f64dc22dfe45067fb88584181966e93342b046
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-08-22 19:20:48 -0300

    correções

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/**  alert('Boas vindas ao jogo do número secreto');
+alert('Boas vindas ao jogo do número secreto');
 let numeroMaximo = 10;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 
@@ -7,15 +7,15 @@ let tentativas = 1;
 
 //enquanto chute não for igual ao numero secreto
 while (chute != numeroSecreto) {
-    // chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (numeroSecreto == chute) {
         break;
     } else {
         if (chute > numeroSecreto) {
-            // alert(`O numero secreto é menor que ${chute}`);
+            alert(`O numero secreto é menor que ${chute}`);
         } else {
-            // alert(`O numero secreto é maior que ${chute}`);
+            alert(`O numero secreto é maior que ${chute}`);
         }
         // tentativas = tentativas + 1;
         tentativas++;
@@ -23,11 +23,10 @@ while (chute != numeroSecreto) {
 }
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
-//alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-console.log(numeroSecreto)
-// if (tentativas > 1) {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);    
-// }
-*/
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+
+if (tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}
